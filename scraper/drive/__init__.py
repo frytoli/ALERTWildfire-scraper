948508770bd8d5e305580d327eb55fa76a788d56
frytoli <>
2021-08-24 13:14:36 -0500

    improved secrets organization and credential logic

--- scraper/drive/__init__.py
+++ scraper/drive/__init__.py
@@ -10,33 +10,49 @@ import os
 
 class gdrive():
     def __init__(self):
-        # Set initial raw creds
-        self.raw_creds = {
-            'token': os.environ['TOKEN'],
-            'refresh_token': os.environ['REFRESH_TOKEN'],
-            'token_uri': os.environ['TOKEN_URI'],
-            'client_id': os.environ['CLIENT_ID'],
-            'client_secret': os.environ['CLIENT_SECRET'],
-            'scopes': ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.file'] # Modifying these scopes will require a new token
-        }
+        # Set vars
+        self.CLIENT_ID = os.environ['CLIENT_ID']
+        self.CLIENT_SECRET = os.environ['CLIENT_SECRET']
+        self.PROJECT_ID = os.environ['PROJECT_ID']
+        self.TOKEN_URI = 'https://oauth2.googleapis.com/token'
+        self.AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+        self.AUTH_PROVIDER_X509_CERT_URL = 'https://www.googleapis.com/oauth2/v1/certs'
+        self.REDIRECT_URIS = ['urn:ietf:wg:oauth:2.0:oob', 'http://localhost']
+        self.SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.file'] # Modifying these scopes will require a new token
+        self.PARENT_DIR = os.environ['GDRIVE_PARENT_DIR']
         # Authenticate credentials
         self.creds = Credentials(
-            token=self.raw_creds['token'],
-            refresh_token=self.raw_creds['token'],
-            token_uri=self.raw_creds['token'],
-            client_id=self.raw_creds['token'],
-            client_secret=self.raw_creds['token'],
-            scopes=self.raw_creds['scopes']
+            token=os.environ['TOKEN'],
+            refresh_token=os.environ['REFRESH_TOKEN'],
+            token_uri=self.TOKEN_URI,
+            client_id=self.CLIENT_ID,
+            client_secret=self.CLIENT_SECRET,
+            scopes=self.SCOPES
         )
 
     def _refresh_token(self):
         # If there are no (valid) credentials available, let the user log in.
-        if not self.creds.valid and self.creds.expired and self.creds.refresh_token:
-            creds.refresh(Request())
-            # Update raw creds
-            self.raw_creds = creds.to_json()
-        else:
-            print('New token required')
+        if not self.creds or not self.creds.valid:
+            if self.creds.expired and self.creds.refresh_token:
+                creds.refresh(Request())
+                # Update raw creds
+                #self.raw_creds = creds.to_json()
+            else:
+                flow = InstalledAppFlow.from_client_config(
+                    client_config={
+                        'installed': {
+                            'client_id': self.CLIENT_ID,
+                            'project_id': self.PROJECT_ID,
+                            'auth_uri': self.AUTH_URI,
+                            'token_uri': self.TOKEN_URI,
+                            'auth_provider_x509_cert_url': self.AUTH_PROVIDER_X509_CERT_URL,
+                            'client_secret': self.CLIENT_SECRET,
+                            'redirect_uris': self.REDIRECT_URIS
+                        }
+                    },
+                    scopes=self.SCOPES
+                )
+                creds = flow.run_local_server(port=0)
 
     def upload(self, path_to_file, mimetype='text/plain'):
         # Refresh tokens if necessary
@@ -46,7 +62,7 @@ class gdrive():
         # Define file metadata and file media
         filename = path_to_file.split('/')[-1]
         metadata = {
-            'parents': ['1OcdnicknPOAHsf8yKr43p5mm_hmWkjFm'],
+            'parents': [self.PARENT_DIR],
             'name': filename,
             'mimeType': mimetype
         }
