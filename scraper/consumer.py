1dee17aad6b505447297093a0e77113b7dd5f28b
frytoli <>
2021-08-25 15:19:45 -0500

    implemented asynchronous http requests

--- scraper/consumer.py
+++ scraper/consumer.py
@@ -1,103 +1,115 @@
 #!/usr/bin/env python
 
-from requests.exceptions import ConnectionError, ProxyError
-from requests_html import HTMLSession
+from requests.exceptions import ConnectionError, ProxyError, TooManyRedirects
+from requests_html import AsyncHTMLSession
 from celery import Celery
 import random
 import time
 import os
 
 app = Celery(
-    'consumer',
-    broker=f'''amqp://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@{os.getenv('RABBITMQ_HOST')}:{os.getenv('RABBITMQ_PORT')}''',
-    backend='rpc://redis:6379'
+	'consumer',
+	broker=f'''amqp://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@{os.getenv('RABBITMQ_HOST')}:{os.getenv('RABBITMQ_PORT')}''',
+	backend='rpc://redis:6379'
 )
 
+def make_afunc(asession, id, url, proxy, headers={}, render=False):
+	async def _afunction():
+		try:
+			# Add headers if applicable
+			if len(headers) > 0:
+				r = await asession.get(
+					url,
+					proxies={
+						'http':f'http://{proxy}',
+						'https':f'https://{proxy}'
+					},
+					headers=headers
+				)
+			else:
+				r = await asession.get(
+					url,
+					proxies={
+						'http':f'http://{proxy}',
+						'https':f'https://{proxy}'
+					},
+				)
+			# Render JS
+			if render:
+				await r.html.arender(timeout=20)
+		except (ConnectionError, ProxyError, TooManyRedirects):
+			r = None
+		return r, id, url, proxy
+	return _afunction
+
 @app.task
-def scrape(saveto_dir, id, url, proxies):
-    '''
-        Scrape and save image from a camera url
-    '''
-    print(f'[-] Now scraping {url}')
-    # Sleep randomly
-    time.sleep(random.randint(5,25))
-    # Select proxy pair
-    proxy = random.choice(proxies)
-    # Initialize HTML session
-    session = HTMLSession()
-    # Request page and render JS
-    try:
-        r = session.get(
-            url,
-            proxies={
-                'http':f'http://{proxy}',
-                'https':f'https://{proxy}'
-            }
-        )
-    except (ConnectionError, ProxyError):
-        r = None
-    while not r or r.status_code not in [200, 301, 302, 303, 307]:
-        # Select another proxy pair
-        proxy = random.choice(proxies)
-        # Sleep for short, random time
-        time.sleep(random.randint(3,13))
-        try:
-            r = session.get(
-                url,
-                proxies={
-                    'http':f'http://{proxy}',
-                    'https':f'https://{proxy}'
-                }
-            )
-        except (ConnectionError, ProxyError):
-            r = None
-    r.html.render(timeout=20)
-    print(f'  [-] Page rendered')
-    # Find camera image source
-    src = r.html.find('.leaflet-image-layer', first=True).attrs['src']
-    img = f'http:{src}'
-    print(f'  [-] Image found at {img}')
-    # Sleep for short, random time
-    time.sleep(random.randint(3,13))
-    # Download/save image
-    # Include referer in header
-    if 'https' in url:
-        root_domain = f'''https://{url.replace('https://','').split('/')[0]}/'''
-    elif 'http' in url:
-        root_domain = f'''http://{url.replace('http://','').split('/')[0]}/'''
-    else:
-        print('  [!] Incorrect protocol detected')
-    headers = {'Referer': root_domain}
-    try:
-        r = session.get(
-            img,
-            headers=headers,
-            proxies={
-                'http':f'http://{proxy}',
-                'https':f'https://{proxy}'
-            }
-        )
-    except (ConnectionError, ProxyError) as e:
-        r = None
-    while not r or r.status_code != 200:
-        # Select another proxy pair
-        proxy = random.choice(proxies)
-        # Sleep for short, random time
-        time.sleep(random.randint(3,13))
-        try:
-            r = session.get(
-                img,
-                headers=headers,
-                proxies={
-                    'http':f'http://{proxy}',
-                    'https':f'https://{proxy}'
-                }
-            )
-        except (ConnectionError, ProxyError) as e:
-            r = None
-    with open(os.path.join(saveto_dir, f'{id}.jpg'), 'wb') as imgf:
-        imgf.write(r.content)
-    print(f'  [+] Image saved')
-    # Close browser
-    session.close()
-    r.close()
+def scrape(saveto_dir, docs, proxies):
+	'''
+		Scrape and save image from a camera url
+	'''
+	print(f'[-] Now scraping {len(docs)} cameras')
+	# Sleep randomly
+	time.sleep(random.randint(15,30))
+	# Initialize step vars
+	active = {doc['id']: {'url': doc['url'], 'proxy': None, 'step':1} for doc in docs}
+	# Initialize Async HTML session
+	asession = AsyncHTMLSession() #(browser_args=['--proxy-server={proxy}'])
+	# Loop until all good responses are found
+	while len(active) > 0:
+		# Shuffle proxies
+		random.shuffle(proxies)
+		# Craft dynamic arguments for async session
+		aargs = []
+		for id in active:
+			camera = active[id]
+			if camera['step'] == 1: # Step 1
+				proxy = random.choice(proxies)
+				# Make async function for initial page request
+				aargs.append(make_afunc(asession, id, camera['url'], proxy, render=True))
+			elif camera['step'] == 2: # Step 2
+				# Make async function for image page request
+				aargs.append(make_afunc(asession, id, camera['url'], camera['proxy'], headers={'Referer': 'http://www.alertwildfire.org/'}))
+		# Make async requests and pair with id
+		results = asession.run(*aargs)
+		# Iterate over results and complete tasks per step if previous request was successful
+		for result in results:
+			# Parse result
+			r = result[0]
+			id = result[1]
+			url = result[2]
+			proxy = result[3]
+			step = active[id]['step']
+			if r and r.status_code in [200, 301, 302, 303, 307]:
+				print(f'  [-] Good response from {url}')
+				# Step 1
+				if step == 1:
+					# Find direct image url
+					src = r.html.find('.leaflet-image-layer', first=True).attrs['src']
+					img = f'http:{src}'
+					# Update record
+					active[id]['url'] = img
+					active[id]['proxy'] = proxy
+					active[id]['step'] = 2
+				# Step 2
+				elif step == 2:
+					# Save image to file
+					with open(os.path.join(saveto_dir, f'{id}.jpg'), 'wb') as imgf:
+						imgf.write(r.content)
+					print(f'    [+] {id}.jpg saved')
+					# Update record, aka remove it from the
+					del active[id]
+			# If not successful
+			else:
+				print(f'  [-] Bad response from {url}')
+				# Step 1
+				if step == 1:
+					# Select a new proxy and update the record
+					active[id]['proxy'] = random.choice(proxies)
+				# Step 2
+				elif step == 2:
+					# Select a new proxy and update the record
+					active[id]['proxy'] = random.choice(proxies)
+			# Sleep randomly
+			time.sleep(random.randint(3,18))
+	# Close browser
+	asession.close()
--- scraper/producer.py
+++ scraper/producer.py
@@ -12,51 +12,56 @@ import db
 import os
 
 def get_proxies():
-    # Initialize HTML session
-    session = HTMLSession()
-    # Request data from proxyscrape API
-    api_resp = session.get(
-        'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
-    ).text
-    # Split response blob into proxy ip:port pairs
-    pairs = api_resp.split('\r\n')
-    # Remove empty values or mis-structured values
-    good_pairs = [x for x in pairs if (x != '' and x.count('.') == 3)]
-    # Return good proxy pairs
-    return good_pairs
+	# Initialize HTML session
+	session = HTMLSession()
+	# Request data from proxyscrape API
+	api_resp = session.get(
+		'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
+	).text
+	# Split response blob into proxy ip:port pairs
+	pairs = api_resp.split('\r\n')
+	# Remove empty values or mis-structured values
+	good_pairs = [x for x in pairs if (x != '' and x.count('.') == 3)]
+	# Close session
+	session.close()
+	# Return good proxy pairs
+	return good_pairs
 
 def produce(invoked, tweet_ids=None):
-    # Name of temporary local directory where images are saved to
-    filename = f'''{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}'''
-    dirname = os.path.join(os.getcwd(), filename)
-    # Initialize db object
-    adb = db.arangodb()
-    # Initialize drive object
-    gd = drive.gdrive()
-    # If directory exists, delete it
-    if os.path.exists(dirname):
-        shutil.rmtree(dirname)
-    # Make a temp directory
-    os.mkdir(dirname)
-    # Retrieve links from 'cameras' collection in database
-    docs = adb.get_docs('cameras')
-    random.shuffle(docs)
-    # Fetch proxies
-    proxies = get_proxies()
-    # Push to queue and wait for all tasks to complete
-    jobs = group([scrape(dirname, doc['id'], doc['url'], proxies) for doc in docs])
-    results = jobs.apply_async()
-    result = results.join()
-    # After queue is done, zip, upload to google drive, and delete locally
-    shutil.make_archive(filename, 'zip', dirname)
-    # Upload zip to Google Drive
-    gd.upload(f'{filename}.zip', mimetype='application/zip')
-    # Delete local directory
-    shutil.rmtree(dirname)
-    # Log event
-    event = {
-        'file': filename,
-        'invoked': invoked,
-        'tweets': tweet_ids
-    }
-    adb.insert_doc('log', event)
+	# Name of temporary local directory where images are saved to
+	filename = f'''{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}'''
+	dirname = os.path.join(os.getcwd(), filename)
+	# Initialize db object
+	adb = db.arangodb()
+	# Initialize drive object
+	gd = drive.gdrive()
+	# If directory exists, delete it
+	if os.path.exists(dirname):
+		shutil.rmtree(dirname)
+	# Make a temp directory
+	os.mkdir(dirname)
+	# Retrieve links from 'cameras' collection in database
+	docs = adb.get_docs('cameras')
+	random.shuffle(docs)
+	# Chunk the docs into n-long groups
+	n = 8
+	chunked_docs = [docs[i:i+n] for i in range(0, len(docs), n)]
+	# Fetch proxies
+	proxies = get_proxies()
+	# Push to queue and wait for all tasks to complete
+	jobs = group([scrape(dirname, chunk, proxies) for chunk in chunked_docs])
+	results = jobs.apply_async()
+	result = results.join()
+	# After queue is done, zip, upload to google drive, and delete locally
+	shutil.make_archive(filename, 'zip', dirname)
+	# Upload zip to Google Drive
+	gd.upload(f'{filename}.zip', mimetype='application/zip')
+	# Delete local directory
+	shutil.rmtree(dirname)
+	# Log event
+	event = {
+		'file': filename,
+		'invoked': invoked,
+		'tweets': tweet_ids
+	}
+	adb.insert_doc('log', event)
