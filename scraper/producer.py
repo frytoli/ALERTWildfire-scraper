d5f39d939363cc7a1ccc036e4b70a36b12291bd9
frytoli <>
2021-09-07 10:26:10 -0500

    get instead of join async jobs, logging, and removing local zip archive after upload to Google Drive

--- scraper/producer.py
+++ scraper/producer.py
@@ -6,6 +6,7 @@ import datetime
 import random
 import shutil
 import drive
+import time
 import db
 import os
 
@@ -16,6 +17,7 @@ def produce():
 	# Name of temporary local directory where images are saved to
 	filename = f'''{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}'''
 	dirname = os.path.join(os.getcwd(), filename)
+	print(f'[-] Scraping commencing. Saving images to {dirname}')
 	# Initialize db object
 	adb = db.arangodb()
 	# Initialize drive object
@@ -33,13 +35,16 @@ def produce():
 	# Push to queue and wait for all tasks to complete
 	args = [scrape.s(dirname, chunk, timeout=1800) for chunk in chunked_docs] # Dynamically create signature object args for group object
 	jobs = group(*args).apply_async()
-	results = jobs.join()
+	results = jobs.get()
+	print('[-] Now saving results\n')
 	# After queue is done, zip, upload to google drive, and delete locally
 	shutil.make_archive(filename, 'zip', dirname)
 	# Upload zip to Google Drive
 	gd.upload(f'{filename}.zip', mimetype='application/zip')
 	# Delete local directory
 	shutil.rmtree(dirname)
+	# Delete local archive
+	os.remove(f'{filename}.zip')
 
 def schedule():
 	# Scrape indefinitely
